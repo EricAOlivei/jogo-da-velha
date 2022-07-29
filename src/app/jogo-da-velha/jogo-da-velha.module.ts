8da77ce4e8fb7a9e0443446b99cd76abc915bbe4
Eric Oliveira <oliveiras.eric2012@gmail.com>
2022-07-29 11:59:42 -0300

    Create module and jogo-da-velha component

---
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
+
+
+
+@NgModule({
+  declarations: [
+    JogoDaVelhaComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    JogoDaVelhaComponent
+  ],
+  providers: [
+  ]
+})
+export class JogoDaVelhaModule { }
